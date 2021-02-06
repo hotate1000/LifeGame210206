a95081c96c9f71f7e8fbc1d7bbf54e04bb32b9e0
hotate1000 <ooo.336.sss@gmail.com>
2021-02-07 01:18:34 +0900

    次へボタンの実装

--- LifeGame.js
+++ LifeGame.js
@@ -1,27 +1,192 @@
 'use strict';
 
-// ブロックの量を指定
-const BLOCK_WIDTH_AMOUNT  = 30;
-const BLOCK_HEIGHT_AMOUNT = 30;
-
+//ブロックの量を指定
+const BLOCK_WIDTH_AMOUNT  = 40;
+const BLOCK_HEIGHT_AMOUNT = 40;
+//次世代の生死判定用
+let geneneration = [];
 main();
 
 // 実行用
 function main() {
   drawArea();
+  create();
+  clickChangeBlock();
+  
+  //進むボタン
+  $('#next').on('click', function() {
+    clickNextButton();
+    judgmentLifeOrDeath(); 
+    console.log("a");
+  });
+  //削除ボタン
+  $('#reset').on('click', function() {
+    clickResetButton();
+  });
+  
+
+
+  $('#return').on('click', function() {
+    for(let y = 0; y < BLOCK_HEIGHT_AMOUNT; y++) {
+      for(let x = 0; x < BLOCK_WIDTH_AMOUNT; x++) {
+        console.log(geneneration[y][x]);
+        
+      }
+    }
+  });
 }
 
 
+//ブロックの描画
 function drawArea() {
   $("table#blocks").empty();
   for(let y = 0; y < BLOCK_HEIGHT_AMOUNT; y++) {
     let block_height = $("<tr>");
     for (let x = 0; x < BLOCK_WIDTH_AMOUNT; x++) {
+      // let block_width = $( "<td id='" + x + "-" + y + "' style=\"background-color : white;\">");
       let block_width = $( "<td id='" + x + "-" + y + "'>");
+      
       block_height.append(block_width);
     }
     block_height.append("</td>");
     block_height.append("</tr>");
     $("table#blocks").append(block_height);
+  }
+}
+
+//クリックにより色変更
+function clickChangeBlock() {
+  $("#blocks>tr>td").on("click", function() {
+    if($(this).css("background-color") == "rgb(255, 255, 255)")
+    {
+      $(this).css("background-color", "rgb(0, 0, 0)");
+    } 
+    else 
+    {
+      $(this).css("background-color", "rgb(255, 255, 255)");
+    } 
+  });
+} 
+
+//世代を進める
+function clickNextButton() {
+  for(let y = 0; y < BLOCK_HEIGHT_AMOUNT; y++) {
+    for(let x = 0; x < BLOCK_WIDTH_AMOUNT; x++) {
+      if($( "#" + x + "-" + y ).css("background-color") == "rgb(255, 255, 255)"){
+        judgmentWhiteBlockLife(x,y);
+      }
+      else if($( "#" + x + "-" + y ).css("background-color") == "rgb(0, 0, 0)"){
+        judgmentBlackBlockLife(x,y);
+      }
+    }
+  }
+}
+
+
+//白ブロック、次世代の生死調査
+function judgmentWhiteBlockLife(x,y) {
+  let count = 0;
+  if($( "#" + (x - 1) + "-" + (y - 1) ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x) + "-" + (y - 1)     ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x + 1) + "-" + (y - 1) ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x - 1) + "-" + (y)     ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x + 1) + "-" + (y)     ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x - 1) + "-" + (y + 1) ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x) + "-" + (y + 1)     ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x + 1) + "-" + (y + 1) ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if(count == 3) {
+    geneneration[y][x] = true;
+  }
+}
+
+//黒ブロック、次世代の生死
+function judgmentBlackBlockLife(x,y) {
+  let count = 0;
+  if($( "#" + (x - 1) + "-" + (y - 1) ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x) + "-" + (y - 1)     ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x + 1) + "-" + (y - 1) ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x - 1) + "-" + (y)     ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x + 1) + "-" + (y)     ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x - 1) + "-" + (y + 1) ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x) + "-" + (y + 1)     ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if($( "#" + (x + 1) + "-" + (y + 1) ).css("background-color") == "rgb(0, 0, 0)"){
+    count++;
+  }
+  if(count == 3 || count == 2) {
+    geneneration[y][x] = true;
+  }
+}
+
+
+
+
+
+// 次世代の生死判定
+function judgmentLifeOrDeath() {
+  
+  for(let y = 0; y < BLOCK_HEIGHT_AMOUNT; y++) {
+    
+    for(let x = 0; x < BLOCK_WIDTH_AMOUNT; x++) {
+        if(geneneration[y][x] == true) 
+        {
+          document.getElementById(x + "-" + y).style.backgroundColor='rgb(0, 0, 0)';
+        }
+        else if(geneneration[y][x] == false)
+        {
+          document.getElementById(x + "-" + y).style.backgroundColor='rgb(255, 255, 255)';
+        }
+        geneneration[y][x] = false;
+    }
+
+  }
+}
+
+
+//画面のリセット
+function clickResetButton() {
+  main();
+}
+
+
+//生死判定用
+function create() {
+  for(let y = 0; y < BLOCK_HEIGHT_AMOUNT; y++) {
+    let blockY = [];
+    for(let x = 0; x < BLOCK_WIDTH_AMOUNT; x++) {
+      let blockX = false;
+      blockY.push(blockX);
+    }
+    geneneration.push(blockY);
+
   }
 }
